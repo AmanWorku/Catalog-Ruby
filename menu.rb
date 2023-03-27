e880e30f4c1224f78ac55a769aacefd551fcaeb4
Salwa99 <s.ritage99@gmail.com>
2023-03-27 22:44:21 +0000

    Fix linter errors

--- menu.rb
+++ menu.rb
@@ -1,26 +1,23 @@
-class main
+class Main
   def display_menu
-     puts "Welcome to our catalog 👋\nSelect an option:"
-     option = [
-         '1 - List all books',
-         '2 - List all music albums',
-         '3 - List all movies',
-         '4 - List all games',
-         '5 - Add a book',
-         '6 - Add a music album',
-         '7 - Add a movie',
-         '8 - Add a game',
-         '9 - Quit'
-     ]
-     puts option
+    puts "Welcome to our catalog 👋\nSelect an option:"
+    options = [
+      '1 - List all books',
+      '2 - List all music albums',
+      '3 - List all movies',
+      '4 - List all games',
+      '5 - Add a book',
+      '6 - Add a music album',
+      '7 - Add a movie',
+      '8 - Add a game',
+      '9 - Quit'
+    ]
+    puts options
   end
 
   def get_input(prompt, type = :to_s)
     print prompt
-    choice = gets.chomp
+    input = gets.chomp
     type == :to_i ? input.to_i : input
   end
 end
-
-
-
