a1b561fc89855d4c8f1d637bbf3c5d60f949b4f5
Salwa99 <s.ritage99@gmail.com>
2023-03-28 09:52:40 +0000

    Fix linter errors

--- item.rb
+++ item.rb
@@ -1,25 +1,25 @@
 require 'date'
 
 class Item
-    attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
+  attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
 
-    def initialize (@id, @genre, @author, @source, @label, @publish_date, @archived)
-       @id = id
-       @genre = genre
-       @author = author
-       @source = source
-       @label = label
-       @publish_date = publish_date
-       @archived = archived
-    end
+  def initialize(id = Random.rand(1..1000))
+    @id = id
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = label
+    @publish_date = nil
+    @archived = false
+  end
 
-    def can_be_archived?
-        (Date.today.year - Date.parse(@publish_date).year) > 10
-    end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
-    def move_to_archive
-        @archived = true if can_be_archived?
-    end
+  private
 
-    private :can_be_archived?
+  def can_be_archived?
+    (Date.today.year - Date.parse(@publish_date).year) > 10
+  end
 end
--- music_album.rb
+++ music_album.rb
@@ -5,15 +5,15 @@ class MusicAlbum < Item
   attr_accessor :name, :genre, :publish_date
   attr_reader :on_spotify
 
-  def initialize(name, publish_date, on_spotify = false)
+  def initialize(name, publish_date, on_spotify: false)
     super(publish_date)
     @name = name
     @on_spotify = on_spotify
   end
 
+  private
+
   def can_be_archived?
     super && @on_spotify
   end
-
-  private :can_be_archived?
 end
