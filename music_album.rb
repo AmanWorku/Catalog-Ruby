5b606db09de562b583e3d92c7a80dbc0b5adcc33
Salwa99 <s.ritage99@gmail.com>
2023-03-28 13:51:31 +0000

    Make changes in class Music Album

--- music_album.rb
+++ music_album.rb
@@ -9,9 +9,18 @@ class MusicAlbum < Item
     @name = name
     @publish_date = Date.parse(publish_date)
     @on_spotify = on_spotify
+    @genres = []
   end
 
   def can_be_archived?
     (Date.today.year - @publish_date.year) > 10 && @on_spotify
+  end
+
+  def genres
+    @genres
+  end
+
+  def add_genre(genre)
+    @genres << genre
   end
 end
