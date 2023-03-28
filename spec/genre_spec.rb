2fec0776a04e8b1db086fd47eaca74805a7e0a52
Salwa99 <s.ritage99@gmail.com>
2023-03-28 13:50:37 +0000

    Make test for class Genre

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,34 +7,34 @@ RSpec.describe Genre do
     @music_album = MusicAlbum.new('Hotel California', '1976-12-08', on_spotify: true)
   end
 
-  it 'instance is an instance of the class' do
+  it 'Check if it is an instance of the class' do
     expect(@genre).to be_instance_of Genre
   end
 
-  it 'has a name attribute' do
+  it 'Check if it has a name attribute' do
     expect(@genre.name).to eq 'Rock'
   end
 
-  it 'has an items attribute' do
+  it 'Check if it has an items attribute' do
     expect(@genre.items).to eq []
   end
 
-  it 'can add an item to its collection' do
+  it 'Check if it can add an item to its collection' do
     @genre.add_item(@music_album)
     expect(@genre.items).to include @music_album
   end
 
-  it 'adds itself as the item\'s genre' do
+  it 'Check if it adds itself as the item\'s genre' do
     @genre.add_item(@music_album)
     expect(@music_album.genre).to eq @genre
   end
 
-  it 'adds the item to its items collection when adding the item' do
+  it 'Check if it adds the item to its items collection' do
     @genre.add_item(@music_album)
     expect(@genre.items).to include @music_album
   end
 
-  it 'adds itself to the item\'s genres collection when adding the item' do
+  it 'Check if it adds itself to the item\'s genres collection' do
     @genre.add_item(@music_album)
     expect(@music_album.genres).to include @genre
   end
