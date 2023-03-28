cb2b440ea23b3c2e79b4761400783876ae07fe8c
AmanWorku <amexworku2014@gmail.com>
2023-03-28 10:51:22 +0300

    Add breaking lines

--- menu.rb
+++ menu.rb
@@ -5,14 +5,15 @@ class Main
       '1 - List all books',
       '2 - List all music albums',
       '3 - List all movies',
+      '===================================='
       '4 - List all genres',
       '5 - List all labels',
       '6 - List all authors',
+      '===================================='
       '7 - Add a book',
       '8 - Add a music album',
       '9 - Add a movie',
-      '10 - Add a game',
-      '11 - Quit'
+      '10 - Quit'
     ]
     puts options
   end
