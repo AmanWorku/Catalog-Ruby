be58f9fed7cd2d7255711e55f9aee519a0d45115
AmanWorku <amexworku2014@gmail.com>
2023-03-28 12:15:05 +0300

    Fix linter errors

--- label.rb
+++ label.rb
@@ -1,9 +1,9 @@
 require_relative 'item'
 
 class Label < Item
-    attr_accessor :title, :color
-    attr_reader :id, :items
-    
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
   def initialize(title, color)
     super(id = Random.rand(1..1000))
     @id = id
