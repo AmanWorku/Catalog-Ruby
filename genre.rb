ac8e77f3029417af7af3f9abaffdd98d63511c59
Salwa99 <s.ritage99@gmail.com>
2023-03-28 10:43:01 +0000

    Fix linter errors

--- genre.rb
+++ genre.rb
@@ -12,11 +12,6 @@ class Genre
   def add_item(item)
     @items.push(item)
     item.genre = self
-  end
-
-  def add_item(item)
-    @items.push(item)
-    item.genre = self
     item.add_genre(self)
   end
 end
