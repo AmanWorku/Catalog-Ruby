d76737edd8be6c5bd37f174b037db1b55f3e098c
AmanWorku <amexworku2014@gmail.com>
2023-03-29 13:24:55 +0300

    Implement a label table creating sql in schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,9 +2,15 @@ CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   publisher VARCHAR,
   cover_state VARCHAR,
-  genre_id INT NULL REFERENCES genres(id),
-  author_id INT NULL REFERENCES authors(id),
-  label_id INT NULL REFERENCES labels(id),
+  genre_id INT NULL REFERENCES genres(id), -- To be created later
+  author_id INT NULL REFERENCES authors(id),-- To be created later
+  label_id INT NULL REFERENCES labels(id),-- To be created later
   publish_date DATE NOT NULL,
   archived BOOLEAN NOT NULL
+);
+
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  color VARCHAR(50) NOT NULL
 );
