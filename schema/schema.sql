77fff28ac768273757af60f26a703a5f88a9f991
AmanWorku <amexworku2014@gmail.com>
2023-03-29 13:23:18 +0300

    Implement a book table creating sql in schema.sql

---
+++ schema/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR,
+  cover_state VARCHAR,
+  genre_id INT NULL REFERENCES genres(id),
+  author_id INT NULL REFERENCES authors(id),
+  label_id INT NULL REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL
+);
