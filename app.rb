d44ea9eaa1d91b18ebf8f7625e50ecd82079c5b7
AmanWorku <amexworku2014@gmail.com>
2023-03-29 11:49:15 +0300

    Add a method to list all labels

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require_relative 'book'
+require_relative 'label'
 require_relative 'book_methods'
 
 books = []
+label = []
 
+def list_labels(labels)
+    puts "Listing all labels:"
+    labels.each do |label|
+      puts "Label #{label.id}: #{label.title} (#{label.color})"
+    end
+  end
+
+  def add_book(books)
+    puts "Enter book publisher:"
+    publisher = gets.chomp
+    puts "Enter cover state (good/bad):"
+    cover_state = gets.chomp
+    book = Book.new(publisher, cover_state)
+    books << book
+    book
+  end
 loop do
     puts "Enter an option:"
     puts "1. Add a book"
@@ -14,8 +32,11 @@ loop do
   case choice
   when '1'
     add_book(books)
+    puts "Book Added successfully"
   when '2'
     list_books(books)
+  when '3'
+    list_labels(labels)
   when 'q'
     break
   else
