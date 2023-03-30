cc31a6b2846904a3abf4238b2fb079e19e5d9e18
AmanWorku <amexworku2014@gmail.com>
2023-03-30 08:51:05 +0300

    Initialize properties

--- game.rb
+++ game.rb
@@ -3,4 +3,10 @@ require 'date'
 
 class Game < Item
 
+    def intialize(multiplayer, last_played_at, publish_date)
+        super(id = Random.rand(1..1000))
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+        @publish_date = publish_date.to
+        @archived = can_be_archived?
 end
