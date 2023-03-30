4ee38df5bd07c4c10e2919847681f86c6ecde98d
AmanWorku <amexworku2014@gmail.com>
2023-03-30 08:57:28 +0300

    Create can_be_archived? method and create accessor

--- game.rb
+++ game.rb
@@ -3,10 +3,20 @@ require 'date'
 
 class Game < Item
 
+    attr_accessor :multiplayer, :last_played_at, :publish_date
+    attr_reader :id
     def intialize(multiplayer, last_played_at, publish_date)
         super(id = Random.rand(1..1000))
         @multiplayer = multiplayer
         @last_played_at = last_played_at
         @publish_date = publish_date.to
         @archived = can_be_archived?
+    end
+
+    def can_be_archived?
+        @archived = if super || Date.today - Date.parse(@last_played_at) > 365 * 2
+            true
+        else
+            false
+        end
 end
