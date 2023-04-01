3969d4542ab432b0dab54e84c1a517df4bc3533a
Salwa99 <s.ritage99@gmail.com>
2023-04-01 19:28:27 +0000

    Wrap up all options in a single menu

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require_relative 'book'
 require_relative 'label'
 require_relative 'book_methods'
+require_relative 'music_album'
+require_relative 'music_album_store'
+require_relative 'game_author'
+require_relative 'game'
 require 'json'
 
 books = []
 labels = []
+
+def get_input(prompt, type = :to_s)
+  print prompt
+  input = gets.chomp
+  type == :to_i ? input.to_i : input
+end
 
 def list_labels(labels)
   puts 'Listing all labels:'
@@ -31,32 +41,84 @@ def save_labels(labels, filename)
   File.write(filename, JSON.generate(labels.map(&:to_h)))
 end
 
-loop do
-  puts 'Enter an option:'
-  puts '1. Add a book'
-  puts '2. List all books'
-  puts '3. Add a label'
-  puts '4. List all labels'
-  puts '5. Exit'
-  choice = gets.chomp.downcase
-
-  case choice
-  when '1'
-    add_book(books)
-    save_books(books, 'data/books.json')
-    puts 'Book Added successfully'
-  when '2'
-    list_books(books)
-  when '3'
-    add_label(labels)
-    save_labels(labels, 'data/labels.json')
-    puts 'Label Added successfully'
-  when '4'
-    list_labels(labels)
-  when '5'
-    puts 'Thanks for using.'
-    break
-  else
-    puts 'Invalid choice. Please try again.'
+def list_books(books)
+  puts 'Listing all books:'
+  books.each do |book|
+    puts "Book #{book.id}: #{book.title} by #{book.author} (#{book.pages} pages)"
   end
+end
+
+def add_book(books)
+  puts 'Enter book title:'
+  title = gets.chomp
+  puts 'Enter book author:'
+  author = gets.chomp
+  puts 'Enter book pages:'
+  pages = gets.chomp.to_i
+  book = Book.new(title, author, pages)
+  books << book
+  book
+end
+
+def book_options
+  loop do
+    puts 'Enter an option:'
+    puts '1. Add a book'
+    puts '2. List all books'
+    puts '3. Add a label'
+    puts '4. List all labels'
+    puts '5. Exit'
+    choice = gets.chomp.downcase
+
+    case choice
+    when '1'
+      add_book(books)
+      save_books(books, 'data/books.json')
+      puts 'Book Added successfully'
+    when '2'
+      list_books(books)
+    when '3'
+      add_label(labels)
+      save_labels(labels, 'data/labels.json')
+      puts 'Label Added successfully'
+    when '4'
+      list_labels(labels)
+    when '5'
+      puts 'Thanks for using.'
+      break
+    else
+      puts 'Invalid choice. Please try again.'
+    end
+  end
+end
+
+puts "Welcome to our catalog 👋\nSelect an option:"
+
+def main_menu
+
+options = [
+  '1 - List books options',
+  '====================================',
+  '2 - List music options',
+  '====================================',
+  '3 - List game options',
+  '====================================',
+  '4 - Quit'
+]
+puts options
+option = gets.chomp.downcase
+case option
+when '1'
+  book_options
+when '2'
+  store = MusicAlbumStore.new
+  store.display_menu
+when '3'
+  game = GameStore.new
+  game.display_menu
+when '4'
+  puts 'Thanks for using.'
+else
+  puts 'Invalid choice. Please try again.'
+end
 end
